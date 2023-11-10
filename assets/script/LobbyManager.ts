5ea1b21ccd4d28bcf51993791ab5abe20f4d4fbb
PiangGG <974805475@qq.com>
2023-11-10 18:15:30 +0800

    add canvs

--- assets/script/LobbyManager.ts
+++ assets/script/LobbyManager.ts
@@ -1,8 +1,31 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Button, Component ,EventHandler , Node , Event} from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Lobby')
 export class Lobby extends Component {
+
+    protected onLoad(): void {
+        console.log("LobbyManager onLoad");
+
+        const clickEventHandler = new EventHandler();
+        clickEventHandler.target = this.node; // 这个 node 节点是你的事件处理代码组件所属的节点
+        clickEventHandler.component = 'Lobby';// 这个是脚本类名
+        clickEventHandler.handler = 'StartGamecallback';
+        clickEventHandler.customEventData = 'foobar';
+
+        const button = this.node.getComponent(Button);
+        button.clickEvents.push(clickEventHandler);
+
+        // if(this.StartButton)
+        // {
+        //     this.StartButton.node.on(Button.EventType.CLICK, this.StartGamecallback, this);
+        //     console.log("开始游戏按钮监听");
+        // }else
+        // {
+        //     console.log("开始游戏按钮监听失败");
+        // }
+    }
+
     start() {
 
     }
@@ -10,5 +33,17 @@ export class Lobby extends Component {
     update(deltaTime: number) {
         
     }
+
+    StartGamecallback(event: Event, customEventData: string) {
+      
+        const node = event.target as Node;
+        const button = node.getComponent(Button);
+        console.log(customEventData);
+    }
+
+    protected onDestroy(): void {
+        
+    }
 }
-
+
+
